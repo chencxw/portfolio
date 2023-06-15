7be8d976ea1960515f8787cb3c8eaf4d1bbb9591
Crystal Chen <cchen435@my.bcit.ca>
2023-06-14 23:05:04 -0700

    added sidebar to desktop sizes

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+import { githubSVG, linkedinSVG, mailSVG } from '../globals/globals';
+
+function Sidebar({restBase}) {
+    const restPath = restBase + 'pages/6?acf_format=standard&test=test';
+    const [restData, setData] = useState([]);
+    const [isLoaded, setLoadStatus] = useState(false);
+
+    // API call
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(restPath)
+            if ( response.ok ) {
+                const data = await response.json()
+                setData(data)
+                setLoadStatus(true)
+            } else {
+                setLoadStatus(false)
+            }
+        }
+        fetchData()
+    }, [restPath])
+
+    return (
+        <>
+        {isLoaded && 
+            <div className='sidebar'>
+                <div className='sidebar-line'></div>
+                <a href={`${restData.acf.contact_email}`}>{mailSVG}</a>
+                <a href={`${restData.acf.github_link}`}>{githubSVG}</a>
+                <a href={`${restData.acf.linkedin_link}`}>{linkedinSVG}</a>
+            </div>}
+        </>
+    )
+}
+export default Sidebar
