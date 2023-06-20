d0f4ebc508b11f04d3ed84ee25c0026c0c136fbc
Crystal Chen <cchen435@my.bcit.ca>
2023-06-19 23:03:13 -0700

    added personal projects to all projects page

--- src/pages/AllProjectsPage.js
+++ src/pages/AllProjectsPage.js
@@ -2,36 +2,45 @@ import {useState, useEffect, useContext} from 'react';
 import ProjectGrid from '../components/ProjectGrid';
 
 function AllProjectsPage({restBase, handleDisplayLoadingGIF, featuredImage}) {
-    const restPathProj = restBase + 'posts?_embed&acf_format=standard&categories=3&order=asc';
+    const restPathProj = restBase + 'posts?_embed&categories=3&order=asc';
+    const restPathPersonalProj = restBase + 'posts?_embed&categories=5';
     const [restDataProj, setDataProj] = useState([]);
+    const [restDataPersonalProj, setDataPersonalProj] = useState([]);
     const [isLoadedProj, setProjLoadStatus] = useState(false);
+    const [isLoadedPersonalProj, setPersonalProjLoadStatus] = useState(false);
 
     // API call
     useEffect(() => {
         handleDisplayLoadingGIF(true);
             const fetchData = async () => {
                 const response_projects = await fetch(restPathProj)
-                if ( response_projects.ok ) {
+                const response_personalProj = await fetch(restPathPersonalProj)
+                if ( response_projects.ok && response_personalProj.ok) {
                     const dataProj = await response_projects.json()
+                    const dataPersonalProj = await response_personalProj.json()
                     setDataProj(dataProj)
+                    setDataPersonalProj(dataPersonalProj)
                     setProjLoadStatus(true)
+                    setPersonalProjLoadStatus(true)
                     setTimeout(() => {
                         handleDisplayLoadingGIF(false);
                     }, 1000)
                 } else {
                     setProjLoadStatus(false)
+                    setPersonalProjLoadStatus(false)
                 }
             }
             fetchData()
-    }, [restPathProj])
+    }, [restPathProj, restPathPersonalProj])
     
     
     return (
         <>
-        {isLoadedProj && 
+        {isLoadedProj && isLoadedPersonalProj &&
             <section className='all-projects project-grid'>
             <h2>All Projects.</h2>
             <ProjectGrid data={restDataProj} featuredImage={featuredImage}/>
+            <ProjectGrid data={restDataPersonalProj} featuredImage={featuredImage}/>
             </section>
         }
         </>
